3bebb7716f84eacb5f350f52d03e370d5fde10bd
RS <sanjaykrishna584@gmail.com>
2022-10-29 10:28:20 +0530

    Added Basic NFT minting

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:stellar_flutter_sdk/stellar_flutter_sdk.dart';
+import 'package:akkredit/functions/create_nft.dart';
+
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,7 +14,11 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("Homepage"),
-    );
+      child:
+          TextButton(
+            child: Text("Tap to create NFT"),
+            onPressed: () => CreateNFT().createTickets(
+                sdk: StellarSDK.TESTNET, network: Network.TESTNET),
+      ));
   }
 }
--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,4 +1,7 @@
+import 'package:cool_alert/cool_alert.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -11,7 +14,21 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("ProfilePage"),
-    );
+      child:TextButton(child:Text("Sign out"),
+                onPressed: () {
+                  CoolAlert.show(
+                      context: context,
+                      type: CoolAlertType.confirm,
+                      onConfirmBtnTap: () {
+                        FirebaseAuth.instance.signOut();
+                        final GoogleSignIn googleSignIn = new GoogleSignIn();
+                        googleSignIn.isSignedIn().then((s) {
+                          googleSignIn.signOut();
+                        });
+                        Navigator.pop(context);
+                      },
+                      text: "Are you sure you want to log out");
+                },
+    ));
   }
 }
